e50e0bf00184acb423919c002a6c30af5ba90fac
Ayesha Aamir <ayeshaaamir38@gmail.com>
2023-04-09 23:54:38 +0500

    routing added..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { FooterComponent } from './footer/footer.component';
     HeaderComponent,
     HomeComponent,
     FooterComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
