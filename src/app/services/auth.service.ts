fdcabbbb72a48fd9063bb635263763ae924bfd50
Ayesha Aamir <ayeshaaamir38@gmail.com>
2023-04-29 21:07:13 +0500

    sign up API integrated..

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,16 +1,18 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  api_base_url: string = 'http://localhost:8081/api/v1/';
 
-  constructor(private http: HttpClient) { }
+  api_base_url: string = 'http://localhost:8080/api/v1';
+
+  constructor(private http: HttpClient) {}
 
   userSignUp(data: any): Observable<any> {
-    return this.http.post(`${this.api_base_url}/registration`, data);
+    const url = `${this.api_base_url}/registration`;
+    return this.http.post(url, data, { responseType: 'text' });
   }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css'],
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, OnDestroy {
   signUpCredentials = new FormGroup({
     firstName: new FormControl('', [Validators.required]),
     lastName: new FormControl('', [Validators.required]),
@@ -16,10 +19,17 @@ export class SignupComponent implements OnInit {
     password: new FormControl('', [Validators.required]),
   });
   isProcess: boolean = false;
+  signUpSubscription: Subscription | undefined;
 
-  constructor(private auth: AuthService, private toast: ToastrService) {}
+  constructor(private auth: AuthService, private toast: ToastrService, private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {}
+
+  ngOnDestroy() {
+    if (this.signUpSubscription) {
+      this.signUpSubscription.unsubscribe();
+    }
+  }
 
   get firstName() {
     return this.signUpCredentials.get('firstName');
@@ -37,30 +47,33 @@ export class SignupComponent implements OnInit {
     return this.signUpCredentials.get('password');
   }
 
-  userSignUp() {
+  handleUserSignUp() {
     this.isProcess = true;
     const data = this.signUpCredentials.value;
+    data.password = data.password;
+
     this.auth.userSignUp(data).subscribe(
-      (res) => {
-        if (res.success) {
-          this.isProcess = false;
-          this.toast.success('', 'User registered successfully', {
-            timeOut: 3000,
-          });
-        } else {
-          this.isProcess = false;
-          this.toast.error('', `${res.message}`, {
-            timeOut: 3000,
-          });
-        }
-        this.signUpCredentials.reset();
-      },
-      (err) => {
+      (response: string) => {
         this.isProcess = false;
-        this.toast.error('', 'Server Error', {
+        this.toast.success('', 'User registered successfully', {
+          timeOut: 8000,
+          positionClass: 'toast-bottom-right',
+        });
+        this.signUpCredentials.reset();
+        this.navigateToLogIn();
+      },
+      (error: HttpErrorResponse) => {
+        this.isProcess = false;
+        console.log(error);
+        this.toast.error('', `Server Error`, {
           timeOut: 3000,
+          positionClass: 'toast-bottom-right',
         });
       }
     );
+  }
+
+  navigateToLogIn() {
+    this.router.navigate(['/login']);
   }
 }
