20fc8f2427c6450b108b10c3d448bdacf05ab762
Ayesha Aamir <ayeshaaamir38@gmail.com>
2023-04-29 21:58:33 +0500

    log in API integrated..

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,41 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-
+  private isLoggedIn: boolean = false;
   api_base_url: string = 'http://localhost:8080/api/v1';
 
   constructor(private http: HttpClient) {}
 
+  isAuthenticated(): boolean {
+    return this.isLoggedIn;
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
   userSignUp(data: any): Observable<any> {
     const url = `${this.api_base_url}/registration`;
     return this.http.post(url, data, { responseType: 'text' });
+  }
+
+  userLogIn(data: any): Observable<any> {
+    const url = `${this.api_base_url}/login`;
+    return this.http.post(url, data, { responseType: 'text' }).pipe(
+      tap((token: string) => {
+        localStorage.setItem('token', token);
+        this.isLoggedIn = true;
+      })
+    );
+  }
+
+  userLogOut(): void {
+    localStorage.removeItem('token');
+    this.isLoggedIn = false;
   }
 }
