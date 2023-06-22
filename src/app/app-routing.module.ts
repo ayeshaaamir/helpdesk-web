563d95085f2878b6cce60a00ea15b31e043998c1
Ayesha Aamir <ayeshaaamir38@gmail.com>
2023-06-22 16:20:57 +0500

    code cleaned..

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,22 +5,14 @@ import { ForgotPasswordComponent } from './components/forgot-password/forgot-pas
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
-import { AdminLoginComponent } from './components/admin-login/admin-login.component';
 import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'admin-login', component: AdminLoginComponent},
   {path: 'sign-up', component: SignupComponent},
   {path: 'forgot-password', component: ForgotPasswordComponent},
   {path: '', redirectTo: '/login', pathMatch: 'full'},
-  {
-    path: 'admin',
-    canActivate: [AuthGuard],
-    loadChildren: () =>
-      import('./modules/admin/admin.module').then((m) => m.AdminModule),
-  },
   {
     path: 'user',
     canActivate: [AuthGuard],
